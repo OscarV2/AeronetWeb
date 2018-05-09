ee0992ae2bf9a07a62af69b500a6ca8a6593317a
OscarV2 <ing.odvega@gmail.com>
2018-05-09 16:35:51 -0500

    mostrar logo en login

--- application/views/login.php
+++ application/views/login.php
@@ -8,6 +8,8 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Qualis</title>
+
+    <link rel="icon" href="<?php echo base_url('assets/img/logo.jpg')?>">
     <!-- Bootstrap core CSS-->
 
     <link href="<?php echo base_url();?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">

629da052faec5300178703184dd77aa4b84eb951
OscarV <ing.odvega@gmail.com>
2019-02-05 15:28:56 -0500

    setting url for vps

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/AeronetWeb/';
-
+//$config['base_url'] = 'http://www.smartbill.us:81/AeronetWeb/';
 /*
 |--------------------------------------------------------------------------
 | Index File
